2333fd664739e8eae20caae4ed836a7ce04a155d
reiven <reiven@gmail.com>
2014-06-05 17:26:07 -0300

    changed allow_testnet to magicbyte to support multisig or altcoins address validation, still need to add correct test

--- bitcoinaddress/__init__.py
+++ bitcoinaddress/__init__.py
@@ -1,5 +1,5 @@
-"""\
-Copied from: 
+"""
+Copied from:
 http://rosettacode.org/wiki/Bitcoin/address_validation#Python
 """
 import string
@@ -10,19 +10,20 @@ digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 class IllegalCharacterError(ValueError):
     """Error class for when a character is not part of base58 """
-    
+
 
 def _bytes_to_long(bytestring, byteorder):
     """For use in python version prior to 3.2 """
     result = []
     if byteorder == 'little':
-        result = (v << i*8 for (i, v) in enumerate(bytestring))
+        result = (v << i * 8 for (i, v) in enumerate(bytestring))
     else:
-        result = (v << i*8 for (i, v) in enumerate(reversed(bytestring)))
+        result = (v << i * 8 for (i, v) in enumerate(reversed(bytestring)))
     return sum(result)
 
+
 def _long_to_bytes(n, length, byteorder):
-    """For use in python version prior to 3.2 
+    """For use in python version prior to 3.2
     Source:
     http://bugs.python.org/issue16580#msg177208
     """
@@ -30,11 +31,12 @@ def _long_to_bytes(n, length, byteorder):
         indexes = range(length)
     else:
         indexes = reversed(range(length))
-    return bytearray((n >> i*8) & 0xff for i in indexes)
+    return bytearray((n >> i * 8) & 0xff for i in indexes)
+
 
 def decode_base58(bitcoin_address, length):
-    """Decode the base58 encoded address. 
-    This form of base58 decoding is Bitcoin specific. 
+    """Decode the base58 encoded address.
+    This form of base58 decoding is Bitcoin specific.
     Be careful outside of Bitcoin context.
     """
     n = 0
@@ -49,6 +51,7 @@ def decode_base58(bitcoin_address, length):
     except AttributeError:
         # Python version < 3.2
         return _long_to_bytes(n, length, 'big')
+
 
 def encode_base58(bytestring):
     """Encode the bytestring to a base58 encode string. """
@@ -70,27 +73,27 @@ def encode_base58(bytestring):
         result += digits58[rest]
         (n, rest) = divmod(n, 58)
     return zeros * '1' + result[::-1]  # reverse string
- 
-def validate(bitcoin_address, allow_testnet=False):
+
+
+def validate(bitcoin_address, magicbyte=False):
     """Check the integrity of the bitcoin address.
-    
+
     Returns False if the address is invalid.
     >>> validate('1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i')
     True
     >>> validate('')
     False
     """
+    if not magicbyte:
+        magicbyte = 0
     clen = len(bitcoin_address)
-    if clen < 27 or clen > 35: # XXX or 34?
-        return False
-    allowed_first = tuple(string.digits)
-    if allow_testnet:
-        allowed_first += ('n', 'm')
-    if not bitcoin_address.startswith(allowed_first):
+    if clen < 27 or clen > 35:  # XXX or 34?
         return False
     try:
         bcbytes = decode_base58(bitcoin_address, 25)
     except IllegalCharacterError:
+        return False
+    if not bcbytes.startswith(chr(int(magicbyte))):
         return False
     # Compare checksum
     checksum = sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
@@ -102,23 +105,4 @@ def validate(bitcoin_address, allow_testnet=False):
 
 
 if __name__ == '__main__':
-    # Playing around mess:
-    n = 2491969579123783355964723219455906992268673266682165637887
-    
-    assert _long_to_bytes(n, 25, 'big') == b'\x00e\xa1`Y\x86J/\xdb\xc7\xc9\x9aG#\xa89[\xc6\xf1\x88\xeb\xc0F\xb2\xff'
-    
-    bc = 'miwxGypTcHDXT3m4avmrMMC4co7XWqbG9r'  # invalid, but still passes!
-    bc = '14oLvT2'  # invalid
-    bc = '1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i'
-    bc = '1111111111111111111114oLvT2'
-    b = _long_to_bytes(n, 25, 'big')
-    print(list(b))
-    #print(n.to_bytes(25, 'big'))
-    print(n)
-    print(_bytes_to_long(b, 'big'))
-    print(encode_base58(decode_base58(bc, 25)))
-    
-    assert validate(bc)
-    #assert not validate( bc.replace('N', 'P', 1) )
-    #assert validate('1111111111111111111114oLvT2')
-    #assert validate("17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j")
+    assert validate("17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j")
